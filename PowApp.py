084b8bcd7b7f75de6af1f57fcd4fafc3b1f9b0f2
powpuphanV.7 <69474469+powpuphan@users.noreply.github.com>
2020-09-17 16:48:22 +0700

    Update PowApp.py

--- PowApp.py
+++ PowApp.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import folium as fo
-from streamlit_folium import folium_static
-import geopandas as gp
+#import folium as fo
+#from streamlit_folium import folium_static
+#import geopandas as gp
+import pydeck as pdk
+import altair as alt
 
-st.title('Streamlit with Folium')
 
-"""
-## An easy way to create a website using Python
-"""
+st.title('''
+Streamlit with Folium By Puphan Pornsilkiat 6030819321
+''')
 
 df1 = pd.read_csv('https://raw.githubusercontent.com/Maplub/odsample/master/20190101.csv')
 df2 = pd.read_csv('https://raw.githubusercontent.com/Maplub/odsample/master/20190102.csv')
@@ -28,40 +29,73 @@ frames = [df1, df2, df3, df4, df5]
 df = pd.concat(frames)
 
 np.random.seed(10)
-remove_n = 200000
+remove_n = 20000
 drop_indices = np.random.choice(df.index, remove_n, replace=False)
 df_subset = df.drop(drop_indices)
 df = df_subset
-
-st.write(df)
-
-
-crs = "EPSG:4326"
-geometry = gp.points_from_xy(df.lonstartl ,df.latstartl)
-geo_df  = gp.GeoDataFrame(df,crs=crs,geometry=geometry)
-
-nan_boundary  = gp.read_file('https://github.com/Maplub/AirQualityData/blob/master/nan_shp_wgs84.zip?raw=true')
-nanall = nan_boundary.unary_union
-
-nan_sta = geo_df.loc[geo_df.geometry.within(nanall)]
+df['timestart']  = pd.to_datetime(df['timestart'])
 
 
-longitude = 100.819200
-latitude = 19.331900
+data = df
 
-station_map = fo.Map(
-	location = [latitude, longitude], 
-	zoom_start = 10)
+DATE_TIME = "timestart"
 
-latitudes = list(nan_sta.latstartl)
-longitudes = list(nan_sta.lonstartl)
-labels = list(nan_sta.name)
+hour = st.slider("Hour to look at", 0, 23)
 
-for lat, lng, label in zip(latitudes, longitudes, labels):
-	fo.Marker(
-		location = [lat, lng], 
-		popup = label,
-		icon = fo.Icon(color='red', icon='heart')
-	).add_to(station_map)
+data = data[data[DATE_TIME].dt.hour == hour]
 
-folium_static(station_map)
+st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+
+st.write("""
+# Map
+""")
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+        latitude=13.687455,
+         longitude=100.536915,
+         zoom=9,
+         pitch=50,
+     ),
+     layers=[
+         pdk.Layer(
+            'HexagonLayer',
+            data=data,
+            get_position='[lonstartl, latstartl]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=data,
+             get_position='[lonstartl, latstartl]',
+             get_color='[200, 30, 200, 160]',
+             get_radius=200,
+         ),
+     ],
+ ))
+
+
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+filtered = data[
+    (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour + 1))
+]
+hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
+
+st.altair_chart(alt.Chart(chart_data)
+    .mark_area(
+        interpolate='step-after',
+    ).encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=['minute', 'pickups']
+    ), use_container_width=True)
+
+if st.checkbox("Show raw data", False):
+    st.subheader("Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+    st.write(data)
